f40cbe8df1d10464315f4b7218345eb99d88749e
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-11-29 02:16:12 +0100

    Manifest that installs flask using pip3

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# Manifest that installs flask using pip3
+# Requirements:
+#	Install flask
+#	version must be 2.1.0
+
+package{ 'install flask':
+  ensure          => '2.1.0',
+  provider        => 'pip3',
+  name            => 'flask',
+  install_options => ['-v'],
+}
