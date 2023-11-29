5be6768088802a93b8eb257466412d5f485b50d5
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-11-29 01:57:09 +0100

    Manifest that creates a file in "/tmp"

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,16 @@
+# Manifest that creates a file in "/tmp"
+# Requirements:
+#	File path is /tmp/school
+#	File permission is 0744
+#	File owner is www-data
+#	File group is www-data
+#	File contains I love Puppet
+
+file{'/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
