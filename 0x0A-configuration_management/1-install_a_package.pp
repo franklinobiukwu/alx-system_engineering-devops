c56ee6f3deaa74fa483ce68dd743e0b1cbcd886e
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-11-29 02:21:51 +0100

    removed 'install_options' property

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,7 +4,6 @@
 #	version must be 2.1.0
 
 package{ 'flask':
-  ensure          => '2.1.0',
-  provider        => 'pip3',
-  install_options => ['-v'],
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
