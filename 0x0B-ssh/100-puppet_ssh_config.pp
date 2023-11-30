a85886288384c7bc1d064265f300e0038ed169d7
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-11-30 10:40:42 +0100

    Changed path from  ~/.ssh/config tp /etc/ssh/ssh_config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,21 +2,15 @@
 
 # Manifest that configure clients SSH
 
-file_line{'Declare host':
-  ensure => 'present'
-  path   => '~/.ssh/config',
-  line   => 'Host 54.90.30.102'
-}
-
 file_line{'Declare Identity File':
   ensure => 'present'
-  path   => '~/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => '	IdentityFile ~/.ssh/school'
 }
 
 filei_line{'Turn off Password':
   ensure => 'present',
-  path   => '~/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => '	PasswordAuthentication no'
 
 }
