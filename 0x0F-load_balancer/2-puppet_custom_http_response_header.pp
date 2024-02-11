69827e79335fb23e4c06fea189f54c023c9e0f56
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-02-11 21:33:22 +0100

    FIX #1

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,13 +1,16 @@
 # Script that installs and configures nginx
 
+exec{'update':
+    command => '/usr/bin/apt-get update'
+}
+
 package{'nginx':
-        ensure => 'present',
-        require=> Apt::Update['all'],
+        ensure => 'installed',
 }
 file_line{'add_header':
         path=>'/etc/nginx/nginx.conf',
         match=>'http {',
-        line=>"http {\n\nadd_header X-Served-By \"$::hostname\"",
+        line=>"http {\n\tadd_header X-Served-By \"${hostname}\";",
         require => Package['nginx'],
 }
 service{'nginx':
